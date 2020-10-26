5fb6799ffa3eac4ab02986f197e52f01e947faa7
Taylor Alexander <tlalexander@gmail.com>
2020-10-26 15:33:42 -0700

    add mit license

--- ff_ex_extended.py
+++ ff_ex_extended.py
@@ -1,3 +1,18 @@
+
+"""
+MIT LICENSE:
+https://mit-license.org/
+Copyright © 2020 TLA
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+"""
+
 import asyncio
 from evdev import UInput, categorize, ecodes, AbsInfo, resolve_ecodes, event_factory, list_devices, InputDevice
 import sys
--- ff_launcher.py
+++ ff_launcher.py
@@ -1,7 +1,21 @@
+
+"""
+MIT LICENSE:
+https://mit-license.org/
+Copyright © 2020 TLA
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+"""
+
 import multiprocessing as mp
 import ff_ex_extended
 import motor_test_wheel
-
 
 
 pipe1, pipe2 = mp.Pipe()
